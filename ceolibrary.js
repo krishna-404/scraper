c62a931a219341a824a3ad7849342c59b90a351a
Balkrishna Agarwal <agarwalbalkrishnaindia@gmail.com>
2020-06-12 19:25:56 +0530

    boilerplate html

--- ceolibrary.js
+++ ceolibrary.js
@@ -150,3 +150,68 @@ function scrapeLeader(){
 
 })
 } 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- displays site properly based on user's device -->
+  <meta name="description" content="This is a project for Frontend Mentor Project. This is called as fylo data storage template.">
+  <meta name="author" content="Balkrishna Agarwal">
+  <meta name="keywords" content="Data Storage Template, Fylo, Frontend Mentor">
+  <meta name="application-name" content="Fylo Data Storage">
+  <!-- <meta name="generator" content="node.js"> -->
+
+  <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+  
+  <title>Frontend Mentor | Fylo data storage component</title>
+
+  <!-- Feel free to remove these styles or customise in your own stylesheet 👍 -->
+  <link rel="stylesheet" type="text/css" href="style.css?v=1.0">
+  
+  <!-- <base>Defines the base URL for all linked objects on a page. -->
+  
+</head>
+<body>
+
+  
+
+  You’ve used 815 GB of your storage
+
+  185 GB Left
+  
+  0 GB
+  1000 GB
+  
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+</body>
+
+  <!-- <script src="js/scripts.js" type="text/javascript"></script> -->
+</html>
+
+<!--  <article>	Defines an article.</article>
+      <aside>Defines some content loosely related to the page content.</aside>
+      <details>	Represents a widget from which the user can obtain additional information or controls on-demand.</details>
+      <summary>Defines a summary for the <details> element.</summary>
+      <hgroup>Defines a group of headings.</hgroup>
+      <nav>Defines a section of navigation links.</nav>
+      <section>Defines a section of a document, such as header, footer etc.</section>
+      <span>Defines an inline styleless section in a document.</span>
+      <code>Specifies text as computer code.</code>
+      <cite>Indicates a citation or reference to another source.</cite>
+      <dfn>Specifies a definition.</dfn>
+      <em>Specifies emphasized text.</em>
+      <mark>Represents text highlighted for reference purposes.</mark>
+      <output>Represents the result of a calculation.</output>
+      <progress>Represents the completion progress of a task.</progress>
+      <blockquote>Defines a long quotation.
+      <q>Defines a short inline quotation.</q>
+      <figcaption>Defines a caption or legend for a figure.</figcaption>
+      <source>Defines alternative media resources for the media elements like <audio> or <video>.
+      <time>Represents a time and/or date.</time>
+      <address>Specifies the author's contact information.
+      <acronym>Defines an acronym.
+      <abbr>Defines an abbreviated form of a longer word or phrase. --></abbr>
