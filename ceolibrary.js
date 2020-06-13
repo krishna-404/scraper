099646b8ba7f549df6a510fc38b773bc67347ef6
galdiatorocks <62951687+galdiatorocks@users.noreply.github.com>
2020-06-13 23:37:40 +0530

    for small bugs

--- ceolibrary.js
+++ ceolibrary.js
@@ -1,13 +1,16 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
 const LeaderModel = require("./leader_model");
 
 async function start(){
-    for(let i=1; i<=132; i+=1){ //loop through all the 132 pages of books listings
+    for(let i=1; i<=1; i+=1){ //loop through all the 132 pages of books listings
         const pageDone = await scrapeBooksList('https://www.theceolibrary.com/books/page/' + i);
-        console.log('page no done:', i, pageDone)
+        console.log('page number done:', i, pageDone)
     }
 }
 
-function scrapeBooksList(){ //scrape the books page for listing
+function scrapeBooksList(homeURL){ //scrape the books page for listing
 
     return new Promise((resolve, reject)=> {
 
@@ -20,28 +23,26 @@ function scrapeBooksList(){ //scrape the books page for listing
         if(response.statusCode !== 200){
 
             console.error('response.statusCode, homeURL, response');
-            reject(response.StatusCode);
+            reject(response.statusCode);
         
         } else {
             let $ = cheerio.load(body);
+
+            console.log(homeURL, response.statusCode);
 
             const elements = $('.fcl-entry')
 
             elements.each(async function(i,e) {
                     
                     const card = $(e)
-
                     let book = {};
                                             
                     book.bookLink = card.find('a')
                                         .attr('href');
-                                            
                     book.bookName = card.find('.book-title')
                                         .text();
-                    
                     book.bookImgPath = card .find('.book-cover')
                                             .attr('data-bgset');
-
                     book.bookAuthor = card.find('.book-info')
                                         .children('a')
                                         .text();
@@ -52,7 +53,6 @@ function scrapeBooksList(){ //scrape the books page for listing
                     if (i == elements.length - 1) {
                         resolve(homeURL + ' scrapebooksList done');
                     }
-
             })
         }
     })
@@ -67,47 +67,39 @@ function scrapeBook(book){
                 console.error('scrapeBook err: ', err);
                 reject(err);
             }
-    
             if(response.statusCode !== 200){
-    
                 console.error('response.statusCode, book.bookLink, response');
-                reject(response.StatusCode);
-            
+                reject(response.statusCode);
             } else {
                 let $ = cheerio.load(body);
-
+                console.log(book.bookLink, response.statusCode);
                 const elements =  $('.re-entry');
 
                 elements.each(async function(i,e){
 
                     const card = $(e);
-
                     let leader = {};
                                         
-                    leader.leaderLink = card.find('a')
+                    leader.leaderLink = "https://www.theceolibrary.com/" + card.find('a')
                                         .attr('href');
-
                     leader.leaderName = card.find('strong')
                                         .text();
-                                            
                     leader.leaderSector = card  .find('p')
                                                 .text()
                                                 .replace(/^[" (]/,"")
                                                 .replace(/[)"]$/,"");
-
                     book.quote = card.text();
 
                     let reco = $('.sources-list>li');
-
                     let recoCard = $(reco[i]);
 
                     book.whereRecommended = recoCard.find('a')
                                                     .attr('href')
                                             
-                    leader = await scrapeLeader(leader);
-                    console.log('scrapeLeader done: ', i);
+                    leader = await scrapeLeader(leader).catch(e => console.error(e));
+                    console.log('scrapeLeader done: ', i, leader, book);
 
-                    leader.booksReco= [];
+                    leader.booksReco = [];
                     leader.booksReco.push(book);
 
                     if (i == elements.length - 1) {
@@ -119,34 +111,38 @@ function scrapeBook(book){
     })
 }
 
-function scrapeLeader(){
-    new Promise((resolve, reject)=> {
+function scrapeLeader(leader){
+new Promise((resolve, reject)=> {
+
+    console.log(leader.leaderLink);
 
         request(leader.leaderLink, function(err, response, body){
             if (err) {
-                console.error('leaderLink err: ', err);
+                console.log('leaderLink err: ', err);
                 reject(err);
             }
-    
             if(response.statusCode !== 200){
-    
-                console.error('response.statusCode, leader.leaderLink, response');
-                reject(response.StatusCode);
-            
+                console.log('response.statusCode, leader.leaderLink, response');
+                reject(response.statusCode);
             } else {
                 let $ = cheerio.load(body);
 
+                console.log(leader.leaderLink, response.statusCode);
                 const card =  $('.tag-description');
+
+                if(!card.find('img').attr('src')){
+                    resolve(leader);
+                }
 
                 leader.imagepath = card .find('img')
                                         .attr('src');
-
                 leader.leaderBio  = card;
                 
+                console.log(leader, response);
                 resolve(leader);
-                
             }
         })
-
-})
+    })
 }
+
+module.exports = start()
